77ebbdb91c759ca532bdec05eff270e0ae1ad917
Ivan <sharonov.ivan.web@gmail.com>
2020-02-04 17:20:22 +0400

    returnt as start file
    Signed-off-by: Ivan <sharonov.ivan.web@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,12 +1,7 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const Todo = require('./models/Todo.js')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-// app.post('/api/her', function (req, res) {
-//   res.send('Hello World!');
-// });
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
@@ -20,19 +15,6 @@ async function start () {
   // Build only in dev mode
   if (config.dev) {
     const builder = new Builder(nuxt)
-    const connectBD = mongoose.connect('mongodb+srv://it_rocket:Sinhrofazotron@cluster0-sz7yi.mongodb.net/todos', {
-      useNewUrlParser: true,
-      useFindAndModify: false
-    })
-    // const todo = new Todo ({
-    //   title: 'тестим тодошки 2'
-    // });
-    // await todo.save();
-    const todos = await Todo.find({});
-    app.get('/api/', function (req, res) {
-      res.send('Hello World!');
-      console.log(todos);
-    });
     await builder.build()
   } else {
     await nuxt.ready()
